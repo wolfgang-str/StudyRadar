6379994e7f9f716761b7d0e19b50c87c728c44a1
wolfgang-str <wcstrubbe@gmail.com>
2025-02-16 21:58:21 -0600

    Revert "Basic usrnm/pswd UI in React"
    This reverts commit d6c77d9e61f63c89933f8598eade83b4d01a8a30.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Login from './Login';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Login />
+    <App />
   </React.StrictMode>
 );
 
