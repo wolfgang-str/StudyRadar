c73bcb3af561f3431c3e01084f31b3f86ce5d22a
Minwoo-lee-lee732 <lee732@uwm.edu>
2025-02-17 16:11:30 -0600

    Add password to models

--- StudyRadar/StudyRadar/models.py
+++ StudyRadar/StudyRadar/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Student(models.Model):
     name = models.CharField(max_length=20)
+    password = models.CharField(max_length=128)
     grad_year = models.CharField(max_length=4)
     email = models.EmailField(max_length=254)
     phone = models.CharField(max_length=15)
     major = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
     
 class Course(models.Model):
     name = models.CharField(max_length=20)
