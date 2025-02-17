eeaadb9c11c1b7370119b38c8041535dbadf0b50
Minwoo-lee-lee732 <lee732@uwm.edu>
2025-02-17 16:27:07 -0600

    minor changes

--- StudyRadar/StudyRadar/views.py
+++ StudyRadar/StudyRadar/views.py
@@ -1,5 +1,6 @@
 # from django.views.decorators.csrf import csrf_exempt
 # from django.utils.decorators import method_decorator
+import json
 from django.http import JsonResponse
 from django.views import View
 from StudyRadar.models import Student
