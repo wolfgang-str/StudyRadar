b8fef7f0c3fdd9ffe66ea77769cf67becca5da26
jiahuiyang2003 <yang2345@uwm.edu>
2025-03-12 16:48:30 -0500

    aboutus

---
+++ src/Aboutus.js
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import logo from "./logo.png";
+import React, { useEffect } from 'react';
+import './Aboutus.css';
+
+const About = () => {
+  useEffect(() => {
+    console.log("About Us page loaded successfully.");
+  }, []);
+
+  return (
+    <div>
+      <div className="App-header">
+        <Link to="/login" className="header-link">
+          <img src={logo} className="Login-logo" alt="logo" />
+        </Link>
+      <h1>About Us</h1>
+      <p>
+        We built this website to help UW-Milwaukee students easily connect and form study groups.
+        Our goal is to create a friendly, efficient platform where you can find peers, schedule study sessions, 
+        and work together to succeed academically. We believe that learning together makes us all stronger.
+      </p>
+      <p>
+        Don't have an account? <Link to="/signup">Sign Up</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+
+export default About;
