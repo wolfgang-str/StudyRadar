995ebad297250c27580cb5920eea0befabf0069c
wolfgang-str <wcstrubbe@gmail.com>
2025-04-03 21:19:29 -0500

    added import for details

--- src/GroupDisplay.js
+++ src/GroupDisplay.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './GroupDisplay.css';
+import { Link } from 'react-router-dom';
 
 const GroupDisplay = ({ searchQuery }) => {
   const [studyGroups, setStudyGroups] = useState([]);
