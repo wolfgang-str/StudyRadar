9f1d257ffc4674d2e3becf2e7e4cca8ebf143a01
alingbretson <alingbretson@gmail.com>
2025-04-10 15:31:22 -0500

    doesn't work but wanted to prove I'm actually doing something orz

--- src/GroupSearchBar.js
+++ src/GroupSearchBar.js
@@ -1,18 +1,51 @@
 import React from 'react';
 
+const filterByName = (searchQuery) => {
+  const fullGroups = StudyGroup.Objects.all();
+  if (searchQuery === "") {
+    return fullGroups;
+  }
+  return fullGroups.filter((group) => group.name.toLowerCase().includes(searchQuery.toLowerCase()));
+}
+
 const GroupSearchBar = ({ searchQuery, setSearchQuery }) => {
+  const query = useRef();
+
   const handleSearchChange = (event) => {
     setSearchQuery(event.target.value);
   };
 
+  emptyState = {
+    groups: [],
+    startIndex: 0,
+    progress: 0
+  }
+
+  const [searchState, setSearchState] = useState(emptyState);
+
+  const fetchGroups = (query) => {
+    fetch('http://localhost:8000/api/groups/') //query goes in here somewhere...?
+      .then((res) => res.json())
+      .then((data) => {
+        setSearchState({
+          groups: data,
+          progress: 0,
+          startIndex: 0
+        })
+      })
+  }
+
   const handleSearchSubmit = (event) => {
     event.preventDefault();
+    const queryVal = query.current.value;
+    fetchGroups(queryVal.trim());
   };
 
   return (
     <div style={{ marginBottom: '20px' }}>
       <form onSubmit={handleSearchSubmit}>
         <input
+          ref={query}
           type="text"
           placeholder="Search groups..."
           value={searchQuery}
